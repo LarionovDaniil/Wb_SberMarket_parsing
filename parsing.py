75aed5f01a26dd694b4dd8e2b0ea463d448dfb59
Daniil L <daniil.larionov@gmail.com>
2023-07-19 15:39:33 +0300

    comments

--- parsing.py
+++ parsing.py
@@ -17,13 +17,40 @@ try:
     driver.find_element(By.CLASS_NAME, 'product-review__count-review').click()
     time.sleep(1)
     driver.find_element(By.TAG_NAME, value='body').send_keys(Keys.PAGE_DOWN)
-    time.sleep(0.5)
+    time.sleep(1)
     driver.find_element(By.LINK_TEXT, 'Смотреть все отзывы').click()
-
+    time.sleep(1)
     reviews = open('reviews.txt', 'w')
-    len_strings =0
-    while len_strings < 200:
-        len_strings += 1
+    len_strings = 0
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    count = 0
+    while count <= 200:
+    # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight-1080);")
+
+    # Wait to load page
+        time.sleep(0.5)
+
+    # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    for i in driver.find_elements(By.CLASS_NAME, 'feedback__text'):
+        print(i.text)
+        count += 1
+    print(count)
+    # while len_strings < 200:
+    #     len_strings += 1
+    # print()
+    # time.sleep(5)
+    # for _ in range(5):
+    #     driver.find_element(By.TAG_NAME, value='body').send_keys(Keys.PAGE_DOWN)
+    #     time.sleep(0.5)
+    # time.sleep(1)
+    # for k in driver.find_elements(By.CLASS_NAME, 'feedback__text'):
+    #     print(k.text)
     time.sleep(500)
 
 except Exception as ex:
