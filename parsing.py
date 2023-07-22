914ab2d4974d6ad664f0939a7f00245fac5ada36
Daniil L <daniil.larionov@gmail.com>
2023-07-22 18:40:04 +0300

    sber

--- parsing.py
+++ parsing.py
@@ -33,7 +33,7 @@ def wildberries_parsing(link):
         for _ in range(100):
             driver.find_element(By.TAG_NAME, value='body').send_keys(Keys.PAGE_DOWN)
 
-        with open('reviews.csv', 'w', encoding='utf-8') as file:
+        with open('reviews_wb.csv', 'w', encoding='utf-8') as file:
             for line in driver.find_elements(By.CLASS_NAME, 'feedback__text'):
                 # print(line.text+'$$')
                 file.write(line.text+'$')
@@ -48,10 +48,64 @@ def wildberries_parsing(link):
         driver.quit()
     return 0
 
+
+def sber_parsing(link):
+
+    s = Service(executable_path='webdriver/chromedriver.exe')
+    driver = webdriver.Chrome(service=s)
+
+    try:
+        f = open('reviews_sber.csv', 'w', encoding='utf-8')
+        driver.maximize_window()
+        driver.get(link)
+        time.sleep(0.5)
+        driver.find_element(By.CLASS_NAME, 'reviews-rating__reviews-count').click()
+        time.sleep(0.5)
+        count = 0
+        for i in range(2, 10):
+            # for line in driver.find_elements(By.CLASS_NAME, 'review-item__body-text'):
+            #     print(line.text+'$')
+            try:
+                for line in driver.find_elements(By.CLASS_NAME, 'review-item__body-text'):
+                    f.write(line.text+'$')
+                    count += 1
+                driver.find_element(By.LINK_TEXT, str(i)).click()
+                time.sleep(0.5)
+                print(count)
+            except:
+                pass
+        # driver.find_element(By.CLASS_NAME, 'product-review__count-review').click()
+        # time.sleep(1)
+        # driver.find_element(By.TAG_NAME, value='body').send_keys(Keys.PAGE_DOWN)
+        # time.sleep(0.5)
+        # driver.find_element(By.LINK_TEXT, 'Смотреть все отзывы').click()
+        # time.sleep(1)
+        #
+        # for _ in range(100):
+        #     driver.find_element(By.TAG_NAME, value='body').send_keys(Keys.PAGE_DOWN)
+        #
+        # with open('reviews.csv', 'w', encoding='utf-8') as file:
+        #     for line in driver.find_elements(By.CLASS_NAME, 'feedback__text'):
+        #         # print(line.text+'$$')
+        #         file.write(line.text+'$')
+
+        time.sleep(1)
+
+    except Exception as ex:
+        print(ex)
+        print('Введите полную ссылку на товар с Wildberries')
+    finally:
+        # driver.close()
+        driver.quit()
+    return 0
+
+
 def init():
     url = input()
     if url[12:23] == 'wildberries':
         wildberries_parsing(url)
+    elif url[8:12] == 'sber':
+        sber_parsing(url)
     else:
         print('Введите ссылку именно на товар с Wildberries')
     return 0
